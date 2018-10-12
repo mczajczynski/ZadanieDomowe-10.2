50c4fce367b6d930252b2dc761789378e28e5476
Maciej Czajczynski <m.czajczynski28@gmail.com>
2018-10-12 16:47:02 +0200

    gotowe

---
+++ src/Zadanie.java
@@ -0,0 +1,9 @@
+public class Zadanie {
+
+    public static void main(String[] args) {
+        System.out.println("Wyjątki dzielimy na dwie kategorie które trzeba obsłużyć i inne wyjatki które można, ale nie trzeba obsłużyć.\n" +
+                "Wyjątki nieobslugiwane dziedziczą po klasie RuntimeException\n" +
+                "Wyjątki rzucamy korzystając z instrukcji tchrows, jeśli rzucamy wyjątek kontrolowany musimy dodatkowo w sygnaturze metody/konstruktora zadeklarować wyjątek używając słoa throw\n" +
+                "Do bloku try-catch możemy dodać dodatkowo blok finally, który wykona się zawsze, niezależnie, czy w bloku try wyjątek wystąpił, czy nie");
+    }
+}
